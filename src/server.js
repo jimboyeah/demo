7ca39dcb4e0e026943ebcce7c875bd5044ee8d1a
jimboyeah@gmail.com <jimboyeah@gmail.com>
2019-12-24 01:22:10 +0800

    update package.json

--- src/server.js
+++ src/server.js
@@ -2,16 +2,16 @@ var app = require('http').createServer(handler)
 var io = require('socket.io')(app, { perMessageDeflate: true });
 var fs = require('fs');
 
-app.listen(7001);
+app.listen(8000);
 
 function handler(req, res) {
     var url = req.client.parser.incoming.url;
     console.log(req.client.parser.incoming.url)
     var f = {
-        "/": "/view/socket-client.tpl",
-        "/public/theme.css": "/view/theme.css",
-        "/public/socket.io.js": "/../node_modules/socket.io-client/dist/socket.io.js",
-        "/public/socket.io.js.map": "/../node_modules/socket.io-client/dist/socket.io.js.map",
+        "/": "/view/index.html",
+        "/theme.css": "/view/theme.css",
+        "/socket.io.js": "/../node_modules/socket.io-client/dist/socket.io.js",
+        "/socket.io.js.map": "/../node_modules/socket.io-client/dist/socket.io.js.map",
     } [url]
     fs.readFile(__dirname + (f || url), function(err, data) {
         if (err) {
@@ -25,12 +25,13 @@ function handler(req, res) {
 
 io.on('connection', function(socket) {
     console.log("socket io connection", socket.nsp.name);
-    socket.emit('chat', { hello: 'world' }, (data)=>{
+    socket.emit('chat', { userId:"demo", text: 'hello world' }, (data)=>{
         console.log("emit chat ack", data);    
     });
     socket.on('chat', (data, ack) => {
         var buf = Buffer.from(data, "utf8");
         console.log("socket io on chat", data, ack, buf);
+        socket.emit('chat', {userId:"demo", text:"Echo "+data})
         if (ack && ack.constructor.name == "Function") ack('woo chat');
     });
     socket.on('binary', (data, ack) => {
