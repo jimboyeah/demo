bc84f5f56b3ba0ee3d959425a986fd5d70d72de4
jimboyeah@gmail.com <jimboyeah@gmail.com>
2020-07-01 12:54:43 +0800

    update test

--- autogit.bat
+++ autogit.bat
@@ -1,7 +1,7 @@
 git init
 git remote add origin https://github.com/jimboyeah/demo.git
 git remote show origin
-gti checkout cppDemos
+gti checkout -b cppDemos
 git push origin cppDemos:cppDemos
 rem git push [远程名] [本地分支]:[远程分支]
 
