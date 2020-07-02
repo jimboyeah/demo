c90fdfdec623889d3d5e1e9280c5f1efe9aa0200
jimboyeah@gmail.com <jimboyeah@gmail.com>
2020-07-02 20:35:49 +0800

    edit gitref.bat

--- gitref.bat
+++ gitref.bat
@@ -1,7 +1,10 @@
 git init
 git remote add origin https://github.com/jimboyeah/demo.git
+git status
 git branch -a
+git remote show origin
+
 git pull origin master
-git checkout -b newbranch
-git push origin newbranch:newbranch
+rem git checkout -b OpenGL_demos
+rem git push origin OpenGL_demos:OpenGL_demos
 rem git push [远程名] [本地分支]:[远程分支]
