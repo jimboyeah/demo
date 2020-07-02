7d57354ccb461a83bd86ccbcd3c115c1cb4bed6d
jimboyeah <jimboyeah@gmail.com>
2020-07-02 18:59:54 +0800

    Update autogit.bat

--- autogit.bat
+++ autogit.bat
@@ -2,3 +2,6 @@ git init
 git remote add origin https://github.com/jimboyeah/demo.git
 git branch -a
 git pull origin master
+git checkout -b newbranch
+git push origin newbranch:newbranch
+rem git push [远程名] [本地分支]:[远程分支]
