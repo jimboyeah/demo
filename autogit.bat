3b593f17cb4e1377510d7e53d149349ed141d9d3
jimboyeah@gmail.com <jimboyeah@gmail.com>
2020-07-02 19:00:51 +0800

    add bat for git

---
+++ autogit.bat
@@ -0,0 +1,8 @@
+git init
+git remote add origin https://github.com/jimboyeah/demo.git
+git branch -a
+git pull origin master
+git pull origin dllDemo
+git checkout dllDemo
+git push origin dllDemo:dllDemo
+rem git push [远程名] [本地分支]:[远程分支]
